b942011e6933feb2ac698cf201dbb3bf151ad08d
Jmuddiman666 <12989976+Jmuddiman666@users.noreply.github.com>
2022-07-20 00:50:33 +0100

    Added create button component to add new tasks

---
+++ src/todo.web/src/Components/Create/Create.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { TodoType } from '../../Enums/TodoType';
+import { EndpointApiClient, IEndpointClient } from '../../Services/Endpoints';
+
+export function Create() {
+    const [text, setText] = useState('');
+
+
+    const handleClick: any = (event: any) => {
+        if (text && text.length > 0) {
+
+            const client: IEndpointClient = new EndpointApiClient();
+            client.createTodoItem({ id: 0, description: text, type: TodoType.Pending });
+        }
+    };
+    return (< >
+        <input type="text" data-testid='new-item-text' onChange={e => setText(e.target.value)} />
+        <input type="submit" data-testid='create-button' value='Create' className="cta" onClick={handleClick} /> </>);
+
+}
+
+export default Create;
--- src/todo.web/src/Services/Endpoints.ts
+++ src/todo.web/src/Services/Endpoints.ts
@@ -8,6 +8,11 @@ export interface IEndpointClient {
      * Retrieve an array of todoItems
      * */
     getTodoList(): Promise<ITodoItem[]>;
+    /**
+     * Create a new Todo Item
+     * @param todoItem
+     */
+    createTodoItem(todoItem: ITodoItem): Promise<void>;
 }
 
 export class EndpointApiClient implements IEndpointClient {
@@ -16,7 +21,25 @@ export class EndpointApiClient implements IEndpointClient {
         this.baseUrl = API_URL;
     }
 
+    /**
+     * Create a new Todo Item
+     * @param todoItem
+     */
+    createTodoItem(todoItem: ITodoItem): Promise<void> {
+        let url = this.baseUrl + '/api/todo';
 
+        let options = {
+            method: 'POST',
+            body: JSON.stringify(todoItem)
+        };
+        const response = fetch(url, options);
+        console.info(response);
+        return Promise.resolve();
+    }
+
+    /**
+ * Retrieve an array of todoItems
+ * */
     async getTodoList(): Promise<ITodoItem[]> {
         let url = this.baseUrl + '/api/todo';
         var response = await fetch(url);
